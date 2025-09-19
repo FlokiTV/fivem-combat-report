1d135c61c0513ddf04e752d34c1dd7b792064026
FlokiTV <dawysonw@gmail.com>
2025-09-19 16:30:34 -0300

    chore: add mock data

---
+++ server.lua
@@ -0,0 +1,72 @@
+local Games = {}
+
+Games.data = {
+    ['matchmaking-01'] = {
+        players = {
+            data = {
+                attackers = {
+                    [10] = {
+                        nick = 'MidnightWolf',
+                        group = 'group:10',
+                        leader = true
+                    },
+                    [2] = {
+                        nick = 'BlazeGamer',
+                        group = 'group:4',
+                        leader = false
+                    },
+                    [3] = {
+                        nick = 'SpeedRacer',
+                        group = 'group:4',
+                        leader = false
+                    },
+                    [4] = {
+                        nick = 'ShadowNinja',
+                        group = 'group:4',
+                        leader = true
+                    },
+                    [5] = {
+                        nick = 'PhoenixFire',
+                        group = 'group:4',
+                        leader = false
+                    }
+                },
+                defenders = {
+                    [6] = {
+                        nick = 'ThunderBolt',
+                        group = 'group:6',
+                        leader = true
+                    },
+                    [7] = {
+                        nick = 'GhostRider',
+                        group = 'group:6',
+                        leader = false
+                    },
+                    [8] = {
+                        nick = 'NeonSpectre',
+                        group = 'group:9',
+                        leader = false
+                    },
+                    [9] = {
+                        nick = 'DriftKing',
+                        group = 'group:9',
+                        leader = true
+                    },
+                    [1] = {
+                        nick = 'ViperGT',
+                        group = 'group:1',
+                        leader = true
+                    }
+                }
+            }
+        },
+        rounds = {
+            current = 1,
+            data = {
+                [1] = {
+                    -- Insira os dados da rodada aqui.
+                }
+            }
+        }
+    }
+}
